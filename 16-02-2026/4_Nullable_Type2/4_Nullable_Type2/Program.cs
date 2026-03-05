0c2d3cd600c198a9ba4a2c937ea95c6a4a719c8f
Latiksha Prajapat <latiksha01@gmail.com>
2026-03-05 12:00:32 +0530

    Add files via upload

---
+++ 16-02-2026/4_Nullable_Type2/4_Nullable_Type2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program2
+{
+    static void Main(string[] args)
+    {
+        int? j = null; int? k = 54;
+        int result1 = j ?? 0;
+        int result2 = k ?? 0;
+
+        Console.WriteLine("result1 = {0}, result2 = {1}", result1, result2);
+        Console.ReadLine();
+    }
+}
