a21facc9976128cdc5266a47cbfc5c77f27c3f67
Latiksha Prajapat <latiksha01@gmail.com>
2026-03-05 12:01:42 +0530

    Add files via upload

---
+++ 16-02-2026/6_Sort_String_Array/6_Sort_String_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Sort_String_array
+{
+    public static void Main()
+    {
+        string[] StringArray = new string[5] { "Csharp", "ASP.net", "entity", "ADO.net", "WCF" };
+        Array.Sort(StringArray);
+
+        foreach (string str in StringArray)
+        {
+            Console.Write(str + " ");
+        }
+        Console.ReadLine();
+    }
+
+}
