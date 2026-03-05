dfc349d1e40eb6a222b07af7534ffcba6d23e6c9
Latiksha Prajapat <latiksha01@gmail.com>
2026-03-05 11:59:26 +0530

    Add files via upload

---
+++ 16-02-2026/2_Boxing_Unboxing/2_Boxing_Unboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int marks = 85;
+        Console.WriteLine("Marks : " + marks);
+        object objmarks = marks; //boxing
+        Console.WriteLine("Object Marks : " + objmarks);
+        int unboxmarks = (int)objmarks; //unboxing
+        Console.WriteLine("Unboxed Marks : " + unboxmarks);
+        unboxmarks = unboxmarks + 5;
+        Console.WriteLine("Modified Marks : " + unboxmarks);
+
+    }
+}
