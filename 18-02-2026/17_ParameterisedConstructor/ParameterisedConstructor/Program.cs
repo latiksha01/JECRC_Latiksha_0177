ce379e0587b67e5edd2e22ccb93c3a3689b4a8a3
Latiksha Prajapat <latiksha01@gmail.com>
2026-03-05 12:06:23 +0530

    Add files via upload

---
+++ 18-02-2026/17_ParameterisedConstructor/ParameterisedConstructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class ODLExercise
+{
+    private int number;
+    public int Number
+    {
+        get
+        {
+            return number;
+        }
+    }
+    public ODLExercise()
+    {
+        Random r = new Random();
+        number = r.Next();
+    }
+    public ODLExercise(int seed)
+    {
+        Random r = new Random(seed);
+        number = r.Next();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ODLExercise num = new ODLExercise();
+        Console.WriteLine("static number = " + num.Number);
+        ODLExercise num1 = new ODLExercise(500);
+        Console.WriteLine("static speed = " + num1.Number);
+    }
+}
