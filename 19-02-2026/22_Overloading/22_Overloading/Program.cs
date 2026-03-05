305753dbc1d8a2beb56b211f5028ff0390df7c1e
Latiksha Prajapat <latiksha01@gmail.com>
2026-03-05 12:09:29 +0530

    Add files via upload

---
+++ 19-02-2026/22_Overloading/22_Overloading/Program.cs
@@ -0,0 +1,51 @@
+﻿//Polymorphism(3)
+//Method Overloading - Compile time (early binding)
+//Method Overriding - Run time (late binding) done by using virtual / override keywords
+//Method Hiding
+
+using System;
+
+//--------------------------Method Overloading--------------------------
+class Car
+{
+    public string Name;
+    public int NumberOfDoors;
+    public Car()
+    {
+        Name = "Maruti Suzuki";
+        NumberOfDoors = 4;
+    }
+    public Car(string name, int numberOfDoors)
+    {
+        Name = name;
+        NumberOfDoors = numberOfDoors;
+    }
+    public Car(string name)
+    {
+        Name = name;
+        NumberOfDoors = 0;
+    }
+    public Car(int numberOfDoors)
+    {
+        Name = "";
+        NumberOfDoors = numberOfDoors;
+    }
+}
+
+class Exercise
+{
+    static void Main(string[] args)
+    {
+
+        Car c1 = new Car();
+        Car c2 = new Car(2);
+        Car c3 = new Car("Hyundai i20");
+        Car c4 = new Car("Honda City", 4);
+        Console.WriteLine(c1.NumberOfDoors);
+        Console.WriteLine(c1.Name);
+        Console.WriteLine(c2.NumberOfDoors);
+
+    }
+}
+
+
---
+++ 19-02-2026/23_Overriding/23_Overriding/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class GroupAgent
+{
+    public virtual void show()
+    {
+        Console.WriteLine("GroupAgent Created !!");
+        Console.ReadLine();
+    }
+}
+
+class agent : GroupAgent
+{
+    public override void show()
+    {
+        Console.WriteLine("Agent Created !!");
+        Console.ReadLine();
+    }
+}
+
+class Exercise
+{
+    public static void Main()
+    {
+        GroupAgent g1 = new GroupAgent();
+        g1.show();
+        agent a1 = new agent();
+        a1.show();
+        GroupAgent g2 = new agent();
+        g2.show();
+    }
+}
