8752711e4d832fe23484431a95c9945146715ffc
Latiksha Prajapat <latiksha01@gmail.com>
2026-03-05 12:14:44 +0530

    Add files via upload

---
+++ 20-02-2026/7_StringList/7_StringList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+class String_list
+{
+    static void Main(string[] args)
+    {
+        string order = "    Order#1001 | Laptop | Dell | 75000      ";
+        Console.WriteLine(order);
+
+        string trimOrder = order.Trim();
+        Console.WriteLine(trimOrder);
+
+        Console.WriteLine(order.Length);
+        Console.WriteLine(trimOrder.Length);
+
+        string[] parts = trimOrder.Split('|');
+        Console.WriteLine("------After Split------");
+        foreach (var item in parts)
+        {
+            Console.Write(item.Trim());
+        }
+
+        Console.ReadLine();
+
+
+    }
+}
