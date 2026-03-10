536f0ce4511c34b4dd7380aac10507e4060ef0bf
latiksha01 <latiksha01@gmail.com>
2026-03-10 10:34:30 +0530

    27FEB2026

---
+++ WebApplication2/WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 75000 },
+            new Product { Id = 2, Name = "Mouse", Price = 500 },
+            new Product { Id = 3, Name = "Keyboard", Price = 1500 }
+        };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
